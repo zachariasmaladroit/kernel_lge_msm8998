bd0f87c04f9de2812fba1ae5cc6bf2406df17830
Hong-Mei Li <a21834@motorola.com>
2015-03-27 14:15:25 -0700

    drivers:lmk: Fix null pointer issue
    On some race, the tsk that lmk is using may be deleted from the RB tree
    by other thread, and rb_next would return a NULL if we use this tsk to
    get next. For this case, we need to skip this round of shrink and wait
    for the next turn. Otherwise, tsk would trigger NULL pointer panic.
    
    Change-Id: If6688d94a0b2992556cec0cf1847b58c69a38d8d
    Signed-off-by: Hong-Mei Li <a21834@motorola.com>
    (cherry picked from commit 08261c365f317faf8be57cead59f4bba6cc8e8c5)
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -676,7 +676,7 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 	rcu_read_lock();
 #if defined(CONFIG_ANDROID_LMK_ADJ_RBTREE) && !defined(CONFIG_HSWAP)
 	for (tsk = pick_first_task();
-		tsk != pick_last_task();
+		tsk != pick_last_task() && tsk != NULL;
 		tsk = pick_next_from_adj_tree(tsk)) {
 #else
 	for_each_process(tsk) {
