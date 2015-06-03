51bc6d399186c33e46b6fd1adf1ed53775969555
Hong-Mei Li <a21834@motorola.com>
2015-06-03 14:28:48 -0700

    kernel: avoid adding non-thread-group task to LMK rbtree
    To maintain the task adj RB tree, we add a task to the RB tree when fork,
    and delete it when exit. The place is exactly the same as the linear
    p->tasks list, only when the task is thread_group_leader.
    
    But to handle the oom_score_adj change case, which did not check the
    thread_group_leader, we may del/add a non-leader task to the RB tree.
    Finally leave the task in the RB tree, since we would not really delete
    a non-leader task from the tree. The orphan task would finally be freed,
    and cause later use-after-free panic when accessing RB tree.
    
    Solution:
    Move the rbtree adj_node to signal_struct, which is shared between
    task and all threads. This can make sure we only add one node for
    a thread group.
    
    Change-Id: I1e8dfe490656408863b3726c7bc9e4ee6dc5abc1
    Signed-off-by: Hong-Mei Li <a21834@motorola.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -860,6 +860,10 @@ struct signal_struct {
 	short oom_score_adj;		/* OOM kill score adjustment */
 	short oom_score_adj_min;	/* OOM kill score adjustment min value.
 					 * Only settable by CAP_SYS_RESOURCE. */
+#ifdef CONFIG_ANDROID_LMK_ADJ_RBTREE
+	struct rb_node adj_node;
+#endif
+
 #ifdef CONFIG_HSWAP
 	short reclaimed;
 	long before_time;
@@ -1689,9 +1693,6 @@ struct task_struct {
 #endif
 
 	struct list_head tasks;
-#ifdef CONFIG_ANDROID_LMK_ADJ_RBTREE
-	struct rb_node adj_node;
-#endif
 #ifdef CONFIG_SMP
 	struct plist_node pushable_tasks;
 	struct rb_node pushable_dl_tasks;
