bbd5f70eef46c778ae129789f3a9aede5597e44f
googyanas <googyanas@gmail.com>
2015-09-24 03:27:27 +0100

    [PATCH] Add a hack to throttle orientation sensor input events
    The userspace orientation sensor code in Android ICS seems to constantly
    send updates through the uinput subsystem, which causes the CPU to
    constantly run at 15-20% while the system is awake.  This is almost
    certainly a userspace bug, but since the orientation sensor driver is
    proprietary (ugh), work around this by forcing the driver to sleep after
    each update.
    
    Steven Luo <steven+android@steven676.net>
    
    (cherry picked from commit a7e3bd0b8b472f0397ee5512f7132ef8d5ab3f06)
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>

--- drivers/input/misc/uinput.c
+++ drivers/input/misc/uinput.c
@@ -433,8 +433,7 @@ static int uinput_setup_device(struct uinput_device *udev,
 	return retval;
 }
 
-static ssize_t uinput_inject_events(struct uinput_device *udev,
-				    const char __user *buffer, size_t count)
+static inline ssize_t uinput_inject_event(struct uinput_device *udev, const char __user *buffer, size_t count, __u8 *do_sleep)
 {
 	struct input_event ev;
 	size_t bytes = 0;
@@ -456,7 +455,11 @@ static ssize_t uinput_inject_events(struct uinput_device *udev,
 		bytes += input_event_size();
 	}
 
-	return bytes;
+	/* XXX UGLY HACK to throttle system_server orientation sensor code */
+	if (ev.type == 2 && ev.code == 2) {
+		*do_sleep = 1;
+	}
+	return input_event_size();
 }
 
 static ssize_t uinput_write(struct file *file, const char __user *buffer,
@@ -464,6 +467,7 @@ static ssize_t uinput_write(struct file *file, const char __user *buffer,
 {
 	struct uinput_device *udev = file->private_data;
 	int retval;
+	__u8 do_sleep = 0;
 
 	if (count == 0)
 		return 0;
@@ -473,11 +477,16 @@ static ssize_t uinput_write(struct file *file, const char __user *buffer,
 		return retval;
 
 	retval = udev->state == UIST_CREATED ?
-			uinput_inject_events(udev, buffer, count) :
+			uinput_inject_event(udev, buffer, count, &do_sleep) :
 			uinput_setup_device(udev, buffer, count);
 
 	mutex_unlock(&udev->mutex);
 
+	/* XXX UGLY HACK to throttle system_server orientation sensor code */
+	if (do_sleep) {
+		set_current_state(TASK_INTERRUPTIBLE);
+		schedule_timeout(HZ/5);
+	}
 	return retval;
 }
 
