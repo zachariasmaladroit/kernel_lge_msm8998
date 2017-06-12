f06fea738ee8645745c383234123c79806b73ab6
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2017-06-12 22:51:07 +0200

    ACPI / PM: Clean up device wakeup enable/disable code
    The wakeup.flags.enabled flag in struct acpi_device is not used
    consistently, as there is no reason why it should only apply
    to the enabling/disabling of the wakeup GPE, so put the invocation
    of acpi_enable_wakeup_device_power() under it too.
    
    Moreover, it is not necessary to call
    acpi_enable_wakeup_devices() and acpi_disable_wakeup_devices() for
    suspend-to-idle, so don't do that.
    
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    (cherry picked from commit 235d81a630ca2d39818da96f0c14bc960ffbaeb5)
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>

--- drivers/acpi/sleep.c
+++ drivers/acpi/sleep.c
@@ -662,11 +662,11 @@ static int acpi_freeze_begin(void)
 
 static int acpi_freeze_prepare(void)
 {
-	acpi_enable_wakeup_devices(ACPI_STATE_S0);
 	acpi_enable_all_wakeup_gpes();
 	acpi_os_wait_events_complete();
 	if (acpi_sci_irq_valid())
 		enable_irq_wake(acpi_sci_irq);
+
 	return 0;
 }
 
@@ -699,9 +699,9 @@ static void acpi_freeze_sync(void)
 
 static void acpi_freeze_restore(void)
 {
-	acpi_disable_wakeup_devices(ACPI_STATE_S0);
 	if (acpi_sci_irq_valid())
 		disable_irq_wake(acpi_sci_irq);
+
 	acpi_enable_all_runtime_gpes();
 }
 
