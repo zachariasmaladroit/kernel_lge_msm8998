2d961418c67165fe51823226f1d36d023813dcfe
Pranav Vashi <neobuddy89@gmail.com>
2017-08-08 11:12:02 +0200

    [eval] cpufreq: Add latest impulse governor
    Signed-off-by: andip71 <andreasp@gmx.de>
    (cherry picked from commit 6f9e6d452b64e6252c960e77255bf30e6a23d30f)
    Signed-off-by: Jimbo77 <chupyalcohol@gmail.com>
    (cherry picked from commit c126e023ccc7c2785fd9808bc10e988b78e3d1c3)
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>
    
    Conflicts:
    	drivers/cpufreq/Kconfig
    	include/linux/cpufreq.h

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -540,6 +540,9 @@ extern struct cpufreq_governor cpufreq_gov_schedutil;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLU_ACTIVE)
 extern struct cpufreq_governor cpufreq_gov_blu_active;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_blu_active)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IMPULSE)
+extern struct cpufreq_governor cpufreq_gov_impulse;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_impulse)
 #endif
 
 static inline void cpufreq_policy_apply_limits(struct cpufreq_policy *policy)
