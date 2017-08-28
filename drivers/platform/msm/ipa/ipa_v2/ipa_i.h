d244f86946438277c0d0b43dc2a0d99f2199906a
Mohammed Javid <mjavid@qti.qualcomm.com>
2017-08-29 01:05:46 +0530

    msm: ipa: Cache CNE event
    Provide support to cache CNE event for debugging purpose.
    
    Change-Id: I00d101d3b9a627d7aba3e4524ff301797ea6085e
    Signed-off-by: Mohammed Javid <mjavid@codeaurora.org>
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>
    
    Conflicts:
    	drivers/platform/msm/ipa/ipa_v2/ipa_i.h
    	drivers/platform/msm/ipa/ipa_v3/ipa_i.h
    
    due to 'drivers: ipa: Fix direct references to HZ'

--- drivers/platform/msm/ipa/ipa_v2/ipa_i.h
+++ drivers/platform/msm/ipa/ipa_v2/ipa_i.h
@@ -67,6 +67,8 @@
 
 #define IPA_TIMEOUT(value) (msecs_to_jiffies(value * 1000))
 
+#define IPA_MAX_NUM_REQ_CACHE 10
+
 #define IPADBG(fmt, args...) \
 	do { \
 		pr_debug(DRV_NAME " %s:%d " fmt, __func__, __LINE__, ## args);\
@@ -998,6 +1000,11 @@ struct ipacm_client_info {
 	bool uplink;
 };
 
+struct ipa_cne_evt {
+	struct ipa_wan_msg wan_msg;
+	struct ipa_msg_meta msg_meta;
+};
+
 /**
  * struct ipa_context - IPA context
  * @class: pointer to the struct class
@@ -1199,6 +1206,9 @@ struct ipa_context {
 	u32 ipa_rx_max_timeout_usec;
 	u32 ipa_polling_iteration;
 	bool ipa_uc_monitor_holb;
+	struct ipa_cne_evt ipa_cne_evt_req_cache[IPA_MAX_NUM_REQ_CACHE];
+	int num_ipa_cne_evt_req;
+	struct mutex ipa_cne_evt_lock;
 };
 
 /**
