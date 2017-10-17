9d64a14f19f71bb14ebdf1795bc7afa24e28e906
Christoph Paasch <cpaasch@apple.com>
2017-10-17 15:35:10 -0700

    [eval] mptcp: Check for closed meta-socket before tcp_check_req
    As now request-sockets are full fledged sockets in the hash-table,
    calling mptcp_close() is no more cleaning up the half-open
    request-sockets from new subflows.
    
    These will only be cleaned up as the 3-way handshake finishes. At that
    point, we need to check whether the meta-socket is still in a state
    where it can establish new subflows. If we don't do this, refcounting
    gets messed up and we start accessing garbage.
    
    Fixes: b568f578baca (Merge tag 'v4.4' into mptcp_trunk)
    Signed-off-by: Christoph Paasch <cpaasch@apple.com>
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    (cherry picked from commit 84e18404349ecdc8e3e006da562ef9439d20eb79)
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    
    Conflicts:
    	net/mptcp/mptcp_ipv4.c
    	net/mptcp/mptcp_ipv6.c
    (cherry picked from commit 19f0f7370752cc966a7606bd73026c0f367d83cb)
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>
    
    Conflicts:
    	net/ipv4/tcp_ipv4.c
    
    due to LG specific implementation CONFIG_LGP_DATA_TCPIP_MPTCP

--- net/ipv4/tcp_ipv4.c
+++ net/ipv4/tcp_ipv4.c
@@ -1834,22 +1834,30 @@ process:
 			if (is_meta_sk(sk)) {
 				bh_lock_sock(sk);
 
-				if (sock_owned_by_user(sk)) {
-					skb->sk = sk;
-					if (unlikely(sk_add_backlog(sk, skb,
-									sk->sk_rcvbuf + sk->sk_sndbuf))) {
-						reqsk_put(req);
+				if (!mptcp_can_new_subflow(sk)) {
+					inet_csk_reqsk_queue_drop_and_put(sk, req);
+					bh_unlock_sock(sk);
+					sock_put(sk);
 
-						bh_unlock_sock(sk);
-						NET_INC_STATS_BH(net, LINUX_MIB_TCPBACKLOGDROP);
-						goto discard_and_relse;
-					}
+					return 0;
+				}
+
+					if (sock_owned_by_user(sk)) {
+						skb->sk = sk;
+						if (unlikely(sk_add_backlog(sk, skb,
+										sk->sk_rcvbuf + sk->sk_sndbuf))) {
+							reqsk_put(req);
+
+							bh_unlock_sock(sk);
+							NET_INC_STATS_BH(net, LINUX_MIB_TCPBACKLOGDROP);
+							goto discard_and_relse;
+						}
 
 					reqsk_put(req);
 					bh_unlock_sock(sk);
 
 					return 0;
-				}
+					}
 			}
 			sock_hold(sk);
 			nsk = tcp_check_req(sk, skb, req, false);
