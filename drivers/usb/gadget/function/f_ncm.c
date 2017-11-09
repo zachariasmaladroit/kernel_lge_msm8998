420f8e0b149bdc6d8ad22bf8ee9855b69d137e26
Joe Maples <joe@frap129.org>
2017-11-09 16:20:29 -0500

    micro-optimization: Use DSTRLEN to remove incorrect strlen uses
    strlen is often used incorectly to get the length of strings
    defined at compile time. In these cases, the behavior can be
    replicated with sizeof(X) - 1, which is calculated at compile
    time rather than runtime, reducing overhead. I've created a
    simple macro to replace these instances and applied it to all
    the files compiled into the wahoo kernel.
    
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>
    
    Conflicts:
    	drivers/usb/gadget/function/f_ncm.c
    due to 'CONFIG_LGE_USB_GADGET'
    
    [del]	sound/soc/codecs/tfa/tfa_dsp.c

--- drivers/usb/gadget/function/f_ncm.c
+++ drivers/usb/gadget/function/f_ncm.c
@@ -1487,7 +1487,7 @@ static int ncm_bind(struct usb_configuration *c, struct usb_function *f)
 				      sizeof(ncm->ethaddr));
 #endif
 
-	if (status < 12) { /* strlen("01234567890a") */
+	if (status < 12) { /* DSTRLEN("01234567890a") */
 		ERROR(cdev, "%s: failed to get host eth addr, err %d\n",
 		__func__, status);
 		status = -EINVAL;
