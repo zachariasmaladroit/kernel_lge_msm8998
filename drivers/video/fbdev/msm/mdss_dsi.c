e86f0e37440b2853a100a543dba6be45d313de23
Joe Maples <joe@frap129.org>
2017-11-09 20:39:16 -0500

    [eval] mdss: Add state_notifier hooks
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    (cherry picked from commit 5fb5946387bba70e78963ce135bf5abe9fe7c14a)
    Signed-off-by: Jimbo77 <chupyalcohol@gmail.com>
    (cherry picked from commit 189533b75981edc767c7f92c9b5214b7999855c6)
    Signed-off-by: Jimbo77 <chupyalcohol@gmail.com>
    (cherry picked from commit 7e17b670eb22df0992d9b63cebb85bbe2fecd1dd)
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>
    
    Conflicts:
    	drivers/video/fbdev/msm/mdss_dsi.c
    
    due to 'CONFIG_LGE_LCD_POWER_CTRL'
    
    (cherry picked from commit d3cd7449b476295f39f59916f5ab5558ee4ecd81)
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>

--- drivers/video/fbdev/msm/mdss_dsi.c
+++ drivers/video/fbdev/msm/mdss_dsi.c
@@ -35,6 +35,10 @@
 #include "mdss_dsi_phy.h"
 #include "mdss_dba_utils.h"
 
+#ifdef CONFIG_STATE_NOTIFIER
+#include <linux/state_notifier.h>
+#endif
+
 #define CMDLINE_DSI_CTL_NUM_STRING_LEN 2
 
 #if defined(CONFIG_LGE_DISPLAY_COMMON)
@@ -566,6 +570,9 @@ static int mdss_dsi_panel_power_ctrl(struct mdss_panel_data *pdata,
 #ifdef CONFIG_LGE_LCD_POWER_CTRL
 		}
 #endif
+#ifdef CONFIG_STATE_NOTIFIER
+		state_suspend();
+#endif
 		break;
 	case MDSS_PANEL_POWER_ON:
 		if (mdss_dsi_is_panel_on_lp(pdata))
@@ -580,6 +587,9 @@ static int mdss_dsi_panel_power_ctrl(struct mdss_panel_data *pdata,
 			else
 #endif
 				ret = mdss_dsi_panel_power_on(pdata);
+#ifdef CONFIG_STATE_NOTIFIER
+		state_resume();
+#endif
 		break;
 	case MDSS_PANEL_POWER_LP1:
 	case MDSS_PANEL_POWER_LP2:
