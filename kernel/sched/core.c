5a8e2b1abaf763f7a5f905bf63b80b095b89f019
freak07 <michalechner92@googlemail.com>
2018-03-14 12:38:21 +0100

    sync various files with wahoos eas bringup
    (cherry picked from commit 38f2750d053a772df0434bdb7bf3a3d203907524)
    (cherry picked from commit ff5c27306987952274f9bc5285281ac55e9f85b3)
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -8306,9 +8306,11 @@ void sched_destroy_group(struct task_group *tg)
 void sched_offline_group(struct task_group *tg)
 {
 	unsigned long flags;
+	int i;
 
 	/* end participation in shares distribution */
-	unregister_fair_sched_group(tg);
+	for_each_possible_cpu(i)
+		unregister_fair_sched_group(tg, i);
 
 	spin_lock_irqsave(&task_group_lock, flags);
 	list_del_rcu(&tg->list);
