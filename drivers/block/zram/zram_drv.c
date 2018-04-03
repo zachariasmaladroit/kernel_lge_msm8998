f4a62a3db70eba3836214b1822876fb6bee564b9
Vinayak Menon <vinmenon@codeaurora.org>
2018-04-03 16:32:26 +0530

    [eval|stability] zram: allow zram to allocate CMA pages
    Though zram pages are movable, they aren't allowed to enter
    MIGRATE_CMA pageblocks. zram is not seen to pin pages for
    long which can cause an issue. Moreover allowing zram to
    pick CMA pages can be helpful in cases seen where zram order
    0 alloc fails when there are lots of free cma pages, resulting
    in kswapd or direct reclaim not making enough progress.
    
    Change-Id: Id27d1a31b6cd869ebb025b49b76d2b62ba666ccf
    Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>
    Signed-off-by: Dusan Uveric <dusan.uveric9@gmail.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>
    (cherry picked from commit 79d1108b47e26672e25d14642d41eb8ac598632c)
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>
    
    Conflicts:
    	mm/zsmalloc.c

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -780,7 +780,8 @@ compress_again:
 				__GFP_KSWAPD_RECLAIM |
 				__GFP_NOWARN |
 				__GFP_HIGHMEM |
-				__GFP_MOVABLE);
+				__GFP_MOVABLE |
+				__GFP_CMA);
 	if (!handle) {
 		zcomp_stream_put(zram->comp);
 		zstrm = NULL;
@@ -789,7 +790,7 @@ compress_again:
 
 		handle = zs_malloc(meta->mem_pool, clen,
 				GFP_NOIO | __GFP_HIGHMEM |
-				__GFP_MOVABLE);
+				__GFP_MOVABLE | __GFP_CMA);
 		if (handle)
 			goto compress_again;
 
