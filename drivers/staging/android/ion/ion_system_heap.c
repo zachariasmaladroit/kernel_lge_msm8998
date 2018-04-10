e34fc22f703436253c1a0d23639713199548e596
Prakash Gupta <guptap@codeaurora.org>
2018-04-10 15:13:21 +0530

    ion: ion_system_heap: update supported page-orders for ion pool
    The supported mappings for ARMv8 are 1GB, 2MB, 64KB and 4KB. So 1MB
    allocations from ion pool is not used for ARMv8 section map. Such
    allocations end up being mapped as multiple 64K sections map, while still
    using 1MB contiguous memory. In case of ARMv7s, page-order 9 allocations
    are not used as section map.
    
    Drop page-order 8 ion pool for builds using ARMv8 pagetables and page-order
    9 ion pool for builds using ARMv7s.
    
    Change-Id: Ifff2d8f1cf61ce443311d16c11b8edc191b27a22
    Signed-off-by: Prakash Gupta <guptap@codeaurora.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>
    (cherry picked from commit ec2a7a120d686aed243d9cf2f96cc342349ed3cb)
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>

--- drivers/staging/android/ion/ion_system_heap.c
+++ drivers/staging/android/ion/ion_system_heap.c
@@ -56,7 +56,11 @@ static int highorder_to_index(unsigned int order)
 #endif
 
 #ifndef CONFIG_ALLOC_BUFFERS_IN_4K_CHUNKS
-static const unsigned int orders[] = {9, 8, 4, 0};
+#if defined(CONFIG_IOMMU_IO_PGTABLE_ARMV7S)
+static const unsigned int orders[] = {8, 4, 0};
+#else
+static const unsigned int orders[] = {9, 4, 0};
+#endif
 #else
 static const unsigned int orders[] = {0};
 #endif
