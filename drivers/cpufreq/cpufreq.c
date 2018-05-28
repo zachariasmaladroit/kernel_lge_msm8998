35298617ebac42fbc88554cd2586a3982fc2795b
Sultan Alsawaf <sultanxda@gmail.com>
2018-05-28 16:54:32 -0700

    cpufreq: Don't let userspace change performance cluster min freq
    Userspace's input boosts are way too heavy handed; there's no need to
    boost the performance cluster on touch input, so don't let userspace do so.
    
    Signed-off-by: Sultan Alsawaf <sultanxda@gmail.com>
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -706,6 +706,11 @@ static ssize_t store_##file_name					\
 	int ret, temp;							\
 	struct cpufreq_policy new_policy;				\
 									\
+	if (&policy->object == &policy->min) {				\
+		if (policy->cpu > 3)					\
+			return count;					\
+	}								\
+									\
 	memcpy(&new_policy, policy, sizeof(*policy));			\
 	new_policy.min = policy->user_policy.min;			\
 	new_policy.max = policy->user_policy.max;			\
