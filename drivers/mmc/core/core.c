43799514c2684b64a92485bf8a2c15a10b157423
Veerabhadrarao Badiganti <vbadigan@codeaurora.org>
2018-06-06 19:54:17 +0530

    [eval] mmc: core: Update SD card removal logic based on cd gpio state
    Update SD card removal event processing logic. Instead of pinging
    the card to know the card presence rely on card-detect gpio state.
    
    On multi-card tray designs, the same card-tray would be used for SD
    card and SIM cards. If SD card is placed at the rightmost location
    in the tray, then SIM card may come in contact with SD card power-
    supply while removing the tray. It may result in SIM damage.
    
    For protecting SIM from this issue, in multi-card tray designs,
    a h/w fix done such that pmic gets a notification of SD card
    removal event (through hardwiring) and it turns off the SD card
    voltage regulators immediately. All this will be done much before SD
    card driver starts processing card removal event.
    
    To support this design, SD card driver shouldn't turn-on the
    regulator while processing card removal event. But the present mmc
    driver turns-on regulator (multiple times if the card was in suspend
    state). To avoid turning on SD card regulator in card removal path,
    updating the card removal processing logic is based on card detect
    gpio state.
    
    Change-Id: I13708a60c9378519713ebec8071ae3b130012a93
    Signed-off-by: Veerabhadrarao Badiganti <vbadigan@codeaurora.org>
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>
    
    Conflicts:
    	drivers/mmc/core/core.c
    
    due to 'CONFIG_MMC_BLOCK_DEFERRED_RESUME_WAKE_UP'

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -1880,10 +1880,9 @@ EXPORT_SYMBOL(mmc_start_req);
  */
 void mmc_wait_for_req(struct mmc_host *host, struct mmc_request *mrq)
 {
-#ifdef CONFIG_MMC_BLOCK_DEFERRED_RESUME
 	if (mmc_bus_needs_resume(host))
 		mmc_resume_bus(host);
-#endif
+
 	__mmc_start_req(host, mrq);
 	mmc_wait_for_req_done(host, mrq);
 }
@@ -2336,10 +2335,9 @@ void mmc_get_card(struct mmc_card *card)
 {
 	pm_runtime_get_sync(&card->dev);
 	mmc_claim_host(card->host);
-#ifdef CONFIG_MMC_BLOCK_DEFERRED_RESUME
+
 	if (mmc_bus_needs_resume(card->host))
 		mmc_resume_bus(card->host);
-#endif
 }
 EXPORT_SYMBOL(mmc_get_card);
 
@@ -3308,6 +3306,7 @@ int mmc_resume_bus(struct mmc_host *host)
 {
 	unsigned long flags;
 	int err = 0;
+	int card_present = true;
 
 	if (!mmc_bus_needs_resume(host))
 		return -EINVAL;
@@ -3318,7 +3317,10 @@ int mmc_resume_bus(struct mmc_host *host)
 	spin_unlock_irqrestore(&host->lock, flags);
 
 	mmc_bus_get(host);
-	if (host->bus_ops && !host->bus_dead && host->card) {
+	if (host->ops->get_cd)
+		card_present = host->ops->get_cd(host);
+
+	if (host->bus_ops && !host->bus_dead && host->card && card_present) {
 		mmc_power_up(host, host->card->ocr);
 		BUG_ON(!host->bus_ops->resume);
 		err = host->bus_ops->resume(host);
@@ -4602,7 +4604,7 @@ int mmc_pm_notify(struct notifier_block *notify_block,
 	struct mmc_host *host = container_of(
 		notify_block, struct mmc_host, pm_notify);
 	unsigned long flags;
-	int err = 0;
+	int err = 0, present = 0;
 
 	switch (mode) {
 	case PM_RESTORE_PREPARE:
@@ -4649,6 +4651,9 @@ int mmc_pm_notify(struct notifier_block *notify_block,
 
 		spin_lock_irqsave(&host->lock, flags);
 		host->rescan_disable = 0;
+		if (host->ops->get_cd)
+			present = host->ops->get_cd(host);
+
 #ifdef CONFIG_MMC_BLOCK_DEFERRED_RESUME_WAKE_UP
 		if (!host->trigger_card_event && mmc_bus_manual_resume(host)) {
 			spin_unlock_irqrestore(&host->lock, flags);
@@ -4656,7 +4661,8 @@ int mmc_pm_notify(struct notifier_block *notify_block,
 		}
 #else
 		if (mmc_bus_manual_resume(host) &&
-				!host->ignore_bus_resume_flags) {
+				!host->ignore_bus_resume_flags &&
+				present) {
 			spin_unlock_irqrestore(&host->lock, flags);
 			break;
 		}
