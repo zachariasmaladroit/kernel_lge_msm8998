04bd27d56a5fda1f32d05191e1d3e60b2e6db4fe
Minchan Kim <minchan@google.com>
2018-06-27 22:04:37 +0900

    mm: mm_event: add compaction stat
    This patch adds compaction mm_event stat so that we could keep track
    latency of compaction as well as count of the event.
    
    Under heavy memory fragmentation, high-order page allocation(e.g.
    fork, ION memory allocation) triggers compaction, which is
    another major part of latency. Let's track it down, too.
    
    Bug: 116825025
    Change-Id: Ia3da9324f123ba2542863eafaf72024b5351785b
    Signed-off-by: Minchan Kim <minchan@google.com>
    (cherry picked from commit a92498b7c22fdcf8891277b888e8552932853b9d)
    Signed-off-by: Minchan Kim <minchan@google.com>
    (cherry picked from commit 0909cdfc72f90a390f8d0d5edc3ad7c42bd3bb52)
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>

--- mm/compaction.c
+++ mm/compaction.c
@@ -1498,6 +1498,7 @@ static int compact_zone(struct zone *zone, struct compact_control *cc)
 	unsigned long end_pfn = zone_end_pfn(zone);
 	const int migratetype = gfpflags_to_migratetype(cc->gfp_mask);
 	const bool sync = cc->mode != MIGRATE_ASYNC;
+	ktime_t event_ts;
 
 	ret = compaction_suitable(zone, cc->order, cc->alloc_flags,
 							cc->classzone_idx);
@@ -1536,6 +1537,7 @@ static int compact_zone(struct zone *zone, struct compact_control *cc)
 	}
 	cc->last_migrated_pfn = 0;
 
+	mm_event_start(&event_ts);
 	trace_mm_compaction_begin(start_pfn, cc->migrate_pfn,
 				cc->free_pfn, end_pfn, sync);
 
@@ -1621,6 +1623,7 @@ check_drain:
 	}
 
 out:
+	mm_event_end(MM_COMPACTION, event_ts);
 	/*
 	 * Release free pages and update where the free scanner should restart,
 	 * so we don't leave any returned pages behind in the next attempt.
@@ -1699,6 +1702,7 @@ unsigned long try_to_compact_pages(gfp_t gfp_mask, unsigned int order,
 	int may_perform_io = gfp_mask & __GFP_IO;
 	struct zoneref *z;
 	struct zone *zone;
+	ktime_t event_ts;
 	int rc = COMPACT_DEFERRED;
 	int all_zones_contended = COMPACT_CONTENDED_LOCK; /* init for &= op */
 
@@ -1708,6 +1712,7 @@ unsigned long try_to_compact_pages(gfp_t gfp_mask, unsigned int order,
 	if (!order || !may_enter_fs || !may_perform_io)
 		return COMPACT_SKIPPED;
 
+	mm_event_start(&event_ts);
 	trace_mm_compaction_try_to_compact_pages(order, gfp_mask, mode);
 
 	/* Compact each zone in the list */
@@ -1791,6 +1796,7 @@ break_loop:
 	if (rc > COMPACT_SKIPPED && all_zones_contended)
 		*contended = COMPACT_CONTENDED_LOCK;
 
+	mm_event_end(MM_COMPACTION, event_ts);
 	return rc;
 }
 
