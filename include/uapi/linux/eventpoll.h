a629c44a979e44ada5e63b1a5f11ae0f60ba5df8
zachariasmaladroit <zacharias.maladroit@gmail.com>
2018-07-24 18:05:22 +0200

    Revert "epoll: Drop struct epoll_event definition"
    This reverts commit 99c4750fe5435e8d943d1a3b662b63e5455e4ba6.

--- include/uapi/linux/eventpoll.h
+++ include/uapi/linux/eventpoll.h
@@ -72,7 +72,6 @@
 #define EPOLL_PACKED
 #endif
 
-#ifdef __KERNEL__
 struct epoll_event {
 	__u32 events;
 	__u64 data;
@@ -90,5 +89,4 @@ static inline void ep_take_care_of_epollwakeup(struct epoll_event *epev)
 	epev->events &= ~EPOLLWAKEUP;
 }
 #endif
-#endif /* __KERNEL__ */
 #endif /* _UAPI_LINUX_EVENTPOLL_H */
