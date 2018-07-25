0726a1dbe79ada640f14951c3fd043160c8f1247
zachariasmaladroit <zacharias.maladroit@gmail.com>
2018-07-25 02:56:14 +0200

    drivers/video/fbdev/msm/*, drivers/input/misc/qpnp-power-on, msm_vidc_common: queue work on system_power_efficient_wq
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>

--- drivers/video/fbdev/msm/mdss_dsi_status.c
+++ drivers/video/fbdev/msm/mdss_dsi_status.c
@@ -196,7 +196,8 @@ static int fb_event_callback(struct notifier_block *self,
 
 		switch (*blank) {
 		case FB_BLANK_UNBLANK:
-			schedule_delayed_work(&pdata->check_status,
+			queue_delayed_work(system_power_efficient_wq,
+				&pdata->check_status,
 				msecs_to_jiffies(interval));
 			break;
 		case FB_BLANK_POWERDOWN:
