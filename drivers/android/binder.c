0e54c59cc02834f40827b212ef06943a18bbb28e
Sherry Yang <sherryy@android.com>
2018-08-07 12:57:13 -0700

    android: binder: Rate-limit debug and userspace triggered err msgs
    Use rate-limited debug messages where userspace can trigger
    excessive log spams.
    
    Acked-by: Arve Hjønnevåg <arve@android.com>
    Signed-off-by: Sherry Yang <sherryy@android.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>
    (cherry picked from commit 70938b591c0a256682ea5b6ef14fd7649b16ea2b)
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>
    
    Conflicts:
    	drivers/android/binder.c
    
    additional #include <linux/proc_fs.h>
    on our side

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -71,6 +71,8 @@
 #include <linux/security.h>
 #include <linux/spinlock.h>
 #include <linux/proc_fs.h>
+#include <linux/ratelimit.h>
+
 #include <uapi/linux/android/binder.h>
 #include "binder_alloc.h"
 #include "binder_trace.h"
@@ -162,13 +164,13 @@ module_param_call(stop_on_user_error, binder_set_stop_on_user_error,
 #define binder_debug(mask, x...) \
 	do { \
 		if (binder_debug_mask & mask) \
-			pr_info(x); \
+			pr_info_ratelimited(x); \
 	} while (0)
 
 #define binder_user_error(x...) \
 	do { \
 		if (binder_debug_mask & BINDER_DEBUG_USER_ERROR) \
-			pr_info(x); \
+			pr_info_ratelimited(x); \
 		if (binder_stop_on_user_error) \
 			binder_stop_on_user_error = 2; \
 	} while (0)
