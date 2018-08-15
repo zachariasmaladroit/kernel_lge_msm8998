69026de53bf080b930a530a14879ca157a63e00e
zachariasmaladroit <zacharias.maladroit@gmail.com>
2018-08-15 18:55:14 +0200

    Revert "dm: fix race between dm_get_from_kobject() and __dm_destroy()"
    This reverts commit 4f822e693384ab26fb8abc229c1479584e04b914.

--- drivers/md/dm.c
+++ drivers/md/dm.c
@@ -3517,15 +3517,11 @@ struct mapped_device *dm_get_from_kobject(struct kobject *kobj)
 
 	md = container_of(kobj, struct mapped_device, kobj_holder.kobj);
 
-	spin_lock(&_minor_lock);
-	if (test_bit(DMF_FREEING, &md->flags) || dm_deleting_md(md)) {
-		md = NULL;
-		goto out;
-	}
-	dm_get(md);
-out:
-	spin_unlock(&_minor_lock);
+	if (test_bit(DMF_FREEING, &md->flags) ||
+	    dm_deleting_md(md))
+		return NULL;
 
+	dm_get(md);
 	return md;
 }
 
