d98ed1aa30a15b83ac588b2156d1532c40f14f18
zachariasmaladroit <zacharias.maladroit@gmail.com>
2018-09-15 22:41:51 +0200

    Revert "cpufreq: cpu-boost: don't raise min freq if the current frequency is already >= boost frequency"
    This reverts commit 72c3cd4bd8a52cce9880955a24b602d25d123055.

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -216,9 +216,6 @@ static int boost_adjust_notify(struct notifier_block *nb, unsigned long val,
 		ib_min = min((s->input_boost_min == UINT_MAX ?
 				policy->max : s->input_boost_min), policy->max);
 
-		if (policy->cur >= ib_min)
-			break;
-
 		pr_debug("CPU%u policy min before boost: %u kHz\n",
 			 cpu, policy->min);
 		pr_debug("CPU%u boost min: %u kHz\n", cpu, ib_min);
