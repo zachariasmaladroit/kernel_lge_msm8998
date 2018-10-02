c91c8e153de05dbf1703a83507c0ef813b2ed791
celtare21 <celtare21@gmail.com>
2018-10-02 09:50:48 +0000

    [eval] sdcardfs: Checkout to https://github.com/aosp-mirror/kernel_msm/commit/e88841ac026f1af89841725549224eb2ea929bf3
    As usual CAF has it's own way
    
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>
    
    Conflicts:
    	fs/sdcardfs/packagelist.c
    
    doesn't apply anymore for removed
    
    extension_details->num = extensions_value->num;
    
    line ?
    
    '#ifdef CONFIG_MACH_LGE
    	if (!extensions_value) { // WBT 201799
    		kfree(extension_details);
    		return ERR_PTR(-ENOMEM);
    	}
    '#endif

--- fs/sdcardfs/file.c
+++ fs/sdcardfs/file.c
@@ -62,7 +62,6 @@ static ssize_t sdcardfs_write(struct file *file, const char __user *buf,
 	int err;
 	struct file *lower_file;
 	struct dentry *dentry = file->f_path.dentry;
-	struct inode *inode = d_inode(dentry);
 
 	/* check disk space */
 	if (!check_min_free_space(dentry, count, 0)) {
@@ -74,12 +73,10 @@ static ssize_t sdcardfs_write(struct file *file, const char __user *buf,
 	err = vfs_write(lower_file, buf, count, ppos);
 	/* update our inode times+sizes upon a successful lower write */
 	if (err >= 0) {
-		if (sizeof(loff_t) > sizeof(long))
-			inode_lock(inode);
-		fsstack_copy_inode_size(inode, file_inode(lower_file));
-		fsstack_copy_attr_times(inode, file_inode(lower_file));
-		if (sizeof(loff_t) > sizeof(long))
-			inode_unlock(inode);
+		fsstack_copy_inode_size(d_inode(dentry),
+					file_inode(lower_file));
+		fsstack_copy_attr_times(d_inode(dentry),
+					file_inode(lower_file));
 	}
 
 	return err;
@@ -382,7 +379,6 @@ ssize_t sdcardfs_write_iter(struct kiocb *iocb, struct iov_iter *iter)
 {
 	int err;
 	struct file *file = iocb->ki_filp, *lower_file;
-	struct inode *inode = file->f_path.dentry->d_inode;
 
 	lower_file = sdcardfs_lower_file(file);
 	if (!lower_file->f_op->write_iter) {
@@ -397,12 +393,10 @@ ssize_t sdcardfs_write_iter(struct kiocb *iocb, struct iov_iter *iter)
 	fput(lower_file);
 	/* update upper inode times/sizes as needed */
 	if (err >= 0 || err == -EIOCBQUEUED) {
-		if (sizeof(loff_t) > sizeof(long))
-			inode_lock(inode);
-		fsstack_copy_inode_size(inode, file_inode(lower_file));
-		fsstack_copy_attr_times(inode, file_inode(lower_file));
-		if (sizeof(loff_t) > sizeof(long))
-			inode_unlock(inode);
+		fsstack_copy_inode_size(file->f_path.dentry->d_inode,
+					file_inode(lower_file));
+		fsstack_copy_attr_times(file->f_path.dentry->d_inode,
+					file_inode(lower_file));
 	}
 out:
 	return err;
