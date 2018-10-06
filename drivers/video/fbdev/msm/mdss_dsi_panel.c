84e0dcee188de1b64a09ab90918699f346b69675
Tyler Nijmeh <tylernij@gmail.com>
2018-10-05 18:31:07 -0700

    mdss_dsi_panel: set default brightness floor value
    Since Android reaches a soft-limit of 16, this should be the default
    brightness floor.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>

--- drivers/video/fbdev/msm/mdss_dsi_panel.c
+++ drivers/video/fbdev/msm/mdss_dsi_panel.c
@@ -47,7 +47,7 @@ extern int panel_not_connected;
 
 #define VSYNC_DELAY msecs_to_jiffies(17)
 
-static unsigned int brightness_floor;
+static unsigned int brightness_floor = 16; /* 16 is Android's default lowest user-controlled brightness */
 module_param(brightness_floor, uint, 0644);
 
 DEFINE_LED_TRIGGER(bl_led_trigger);
