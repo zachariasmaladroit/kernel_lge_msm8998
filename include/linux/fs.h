f2c94ec5cd26bbd5b711660d7bbe0ce51d0b39ae
zachariasmaladroit <zacharias.maladroit@gmail.com>
2018-10-07 17:17:35 +0200

    Revert "don't put symlink bodies in pagecache into highmem"
    This reverts commit 5a8d65c48fda1b71669084ea085562e2574ca625.

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -3117,7 +3117,6 @@ static inline bool dir_relax(struct inode *inode)
 }
 
 extern bool path_noexec(const struct path *path);
-extern void inode_nohighmem(struct inode *inode);
 
 extern int device_sidechannel_restrict;
 
