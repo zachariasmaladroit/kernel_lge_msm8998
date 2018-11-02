66c9ce1f486496f3ff7c86dddcf2c0c18935e03d
zachariasmaladroit <zacharias.maladroit@gmail.com>
2018-11-02 21:06:15 +0100

    Revert "zram_drv.c: make zstd default compression algorithm, use lzo as a fallback"
    This reverts commit a6e9011b63b7b6ad92568f07f7b64bd3fc617874.

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -39,11 +39,7 @@ static DEFINE_IDR(zram_index_idr);
 static DEFINE_MUTEX(zram_index_mutex);
 
 static int zram_major;
-#ifdef CONFIG_CRYPTO_ZSTD
-static const char *default_compressor = "zstd";
-#else
 static const char *default_compressor = "lzo";
-#endif
 
 /*
  * We don't need to see memory allocation errors more than once every 1
