0548e16a43c53252d730f67e6d4a5cb2928f9635
Francisco Franco <franciscofranco.1990@gmail.com>
2018-11-06 23:24:39 +0000

    net: we're not debugging shit so stfu with stack dumps
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -2620,7 +2620,7 @@ void netdev_rx_csum_fault(struct net_device *dev)
 {
 	if (net_ratelimit()) {
 		pr_err("%s: hw csum failure\n", dev ? dev->name : "<unknown>");
-		dump_stack();
+		//dump_stack();
 	}
 }
 EXPORT_SYMBOL(netdev_rx_csum_fault);
