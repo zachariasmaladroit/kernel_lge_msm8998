4ee576fdf7dbb15d3e733fa2a3aaa0034c71315b
zachariasmaladroit <zacharias.maladroit@gmail.com>
2018-11-10 21:53:11 +0100

    kernel: Boost CPU to the max when Nova/Lawn chair launcher becomes a top app (addition to 'kernel: Boost CPU to the max when Pixel launcher becomes a top app' by sultanxda)
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -2777,8 +2777,12 @@ static ssize_t __cgroup_procs_write(struct kernfs_open_file *of, char *buf,
 		ret = cgroup_attach_task(cgrp, tsk, threadgroup);
 
 	/* Boost CPU to the max for 500 ms when launcher becomes a top app */
-	if (!memcmp(tsk->comm, "s.nexuslauncher", sizeof("s.nexuslauncher")) &&
-		!memcmp(cgrp->kn->name, "top-app", sizeof("top-app")) && !ret) {
+	if ((!memcmp(tsk->comm, "s.nexuslauncher", sizeof("s.nexuslauncher")) &&
+		!memcmp(cgrp->kn->name, "top-app", sizeof("top-app")) && !ret) ||
+		(!memcmp(tsk->comm, "com.teslacoilsw.launcher", sizeof("com.teslacoilsw.launcher")) &&
+		!memcmp(cgrp->kn->name, "top-app", sizeof("top-app")) && !ret) ||
+		(!memcmp(tsk->comm, "ch.deletescape.lawnchair.plah", sizeof("ch.deletescape.lawnchair.plah")) &&
+		!memcmp(cgrp->kn->name, "top-app", sizeof("top-app")) && !ret)) {
 		cpu_input_boost_kick_max(1000);
 		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 1000);
 	}
