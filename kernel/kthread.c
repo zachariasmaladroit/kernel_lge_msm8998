0992c8339933206cd1efccb5da2e68f6c277bd38
zachariasmaladroit <zacharias.maladroit@gmail.com>
2018-11-10 19:52:26 +0100

    Revert "kthread: Force all non-percpu kthreads onto the power cluster"
    This reverts commit 84ad8f28bb46c8d7afc706c5b05bc141751805e8.

--- kernel/kthread.c
+++ kernel/kthread.c
@@ -401,7 +401,6 @@ struct task_struct *kthread_create_on_cpu(int (*threadfn)(void *data),
 		return p;
 	kthread_bind(p, cpu);
 	/* CPU hotplug need to bind once again when unparking the thread. */
-	p->kthread_per_cpu = true;
 	set_bit(KTHREAD_IS_PER_CPU, &to_kthread(p)->flags);
 	to_kthread(p)->cpu = cpu;
 	return p;
