1a27764ded3557095ae6045f8f657eb37641d9d3
jollaman999 <admin@jollaman999.com>
2018-11-12 15:52:30 +0900

    power: qcom: msm-core: queue work on system_power_efficient_wq

--- drivers/power/qcom/msm-core.c
+++ drivers/power/qcom/msm-core.c
@@ -334,7 +334,7 @@ static __ref int do_sampling(void *data)
 		if (!poll_ms)
 			goto unlock;
 
-		schedule_delayed_work(&sampling_work,
+		queue_delayed_work(system_power_efficient_wq, &sampling_work,
 			msecs_to_jiffies(poll_ms));
 unlock:
 		mutex_unlock(&kthread_update_mutex);
@@ -1082,7 +1082,7 @@ static int msm_core_dev_probe(struct platform_device *pdev)
 	for_each_possible_cpu(cpu)
 		set_threshold(&activity[cpu]);
 
-	schedule_delayed_work(&sampling_work, msecs_to_jiffies(0));
+	queue_delayed_work(system_power_efficient_wq, &sampling_work, msecs_to_jiffies(0));
 	cpufreq_register_notifier(&cpu_policy, CPUFREQ_POLICY_NOTIFIER);
 	pm_notifier(system_suspend_handler, 0);
 	return 0;
