477dd8a0e3aa377ceaed59cf95873b15fe31b240
zachariasmaladroit <zacharias.maladroit@gmail.com>
2018-11-19 20:32:58 +0100

    [later|timer] Revert "kthread: Convert callback to use from_timer()"
    This reverts commit 6d0495e8fa78839384dd65ddca1249cfddabbed4.

--- kernel/kthread.c
+++ kernel/kthread.c
@@ -812,14 +812,15 @@ EXPORT_SYMBOL_GPL(kthread_queue_work);
 /**
  * kthread_delayed_work_timer_fn - callback that queues the associated kthread
  *	delayed work when the timer expires.
- * @t: pointer to the expired timer
+ * @__data: pointer to the data associated with the timer
  *
  * The format of the function is defined by struct timer_list.
  * It should have been called from irqsafe timer with irq already off.
  */
-void kthread_delayed_work_timer_fn(struct timer_list *t)
+void kthread_delayed_work_timer_fn(unsigned long __data)
 {
-	struct kthread_delayed_work *dwork = from_timer(dwork, t, timer);
+	struct kthread_delayed_work *dwork =
+		(struct kthread_delayed_work *)__data;
 	struct kthread_work *work = &dwork->work;
 	struct kthread_worker *worker = work->worker;
 
@@ -850,7 +851,8 @@ void __kthread_queue_delayed_work(struct kthread_worker *worker,
 	struct timer_list *timer = &dwork->timer;
 	struct kthread_work *work = &dwork->work;
 
-	WARN_ON_ONCE(timer->function != (TIMER_FUNC_TYPE)kthread_delayed_work_timer_fn);
+	WARN_ON_ONCE(timer->function != kthread_delayed_work_timer_fn ||
+		     timer->data != (unsigned long)dwork);
 
 	/*
 	 * If @delay is 0, queue @dwork->work immediately.  This is for
