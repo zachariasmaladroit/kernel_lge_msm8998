72b50525fded12965cc9c094b608d1556ff303f6
zachariasmaladroit <zacharias.maladroit@gmail.com>
2018-12-09 22:31:43 +0100

    mdss_fb: boost only if last interaction was within 5000 ms
    This should prevent AOD notification boosting and excessive boosting
    when watching videos.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>
    
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>

--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -5340,8 +5340,12 @@ int mdss_fb_do_ioctl(struct fb_info *info, unsigned int cmd,
 		ret = mdss_fb_mode_switch(mfd, dsi_mode);
 		break;
 	case MSMFB_ATOMIC_COMMIT:
+#ifdef CONFIG_CPU_INPUT_BOOST
+	if (time_before(jiffies, last_input_time + msecs_to_jiffies(5000))) {
 		cpu_input_boost_kick_general(100);
 		devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
+	}
+#endif
 		ret = mdss_fb_atomic_commit_ioctl(info, argp, file);
 		break;
 
