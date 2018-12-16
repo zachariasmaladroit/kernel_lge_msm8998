027768991ee6e31a7519e320d0a800240b5f9ef4
zachariasmaladroit <zacharias.maladroit@gmail.com>
2018-12-16 01:48:34 +0100

    printk: skip syslog_print{_all} functionality if 'printk_mode == 0' (default !)
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -1220,6 +1220,11 @@ static int syslog_print(char __user *buf, int size)
 	struct printk_log *msg;
 	int len = 0;
 
+if (printk_mode == 0) {
+//	kfree(text); // no kfree without kmalloc !
+	return len;
+} else {
+
 	text = kmalloc(LOG_LINE_MAX + PREFIX_MAX, GFP_KERNEL);
 	if (!text)
 		return -ENOMEM;
@@ -1277,12 +1282,18 @@ static int syslog_print(char __user *buf, int size)
 	kfree(text);
 	return len;
 }
+}
 
 static int syslog_print_all(char __user *buf, int size, bool clear)
 {
 	char *text;
 	int len = 0;
 	int attempts = 0;
+
+if (printk_mode == 0) {
+//	kfree(text); // no kfree without kmalloc !
+	return len;
+} else {
 
 	text = kmalloc(LOG_LINE_MAX + PREFIX_MAX, GFP_KERNEL);
 	if (!text)
@@ -1396,6 +1407,7 @@ out:
 
 	kfree(text);
 	return len;
+}
 }
 
 int do_syslog(int type, char __user *buf, int len, int source)
