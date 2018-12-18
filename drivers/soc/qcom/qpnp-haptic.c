d1e65bcd8faf7179a074e6b39a497aacecb79f28
idkwhoiam322 <snkm1999@gmail.com>
2018-12-18 14:18:07 +0300

    fix gcc 8.2.1 warnings
    Signed-off-by: idkwhoiam322 <snkm1999@gmail.com>
    (cherry picked from commit c74d9098b8442cf5298c2068bc63006bd116b7f5)
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>
    
    Conflicts:
    	drivers/char/diag/diagfwd_peripheral.c
    	drivers/media/dvb-core/dmxdev.c
    	drivers/media/platform/msm/camera_v2/sensor/msm_sensor_driver.c
    	drivers/power/supply/qcom/battery_current_limit.c
    NA (O+)	drivers/power/supply/qcom/op_dash_adapter.c
    	drivers/scsi/ufs/ufs_quirks.c
    	drivers/soc/qcom/qpnp-haptic.c
    
    non-matching sections due to:
    'Synchronize codes for OnePlus5 & 5T P OPEN BETA 1'
    
    Conflicts:
    	drivers/media/dvb-core/dmxdev.c
    	drivers/soc/qcom/qpnp-haptic.c

--- drivers/soc/qcom/qpnp-haptic.c
+++ drivers/soc/qcom/qpnp-haptic.c
@@ -2992,7 +2992,7 @@ static int qpnp_haptic_probe(struct platform_device *pdev)
 		return -ENOMEM;
 	hap->regmap = dev_get_regmap(pdev->dev.parent, NULL);
 	if (!hap->regmap) {
-		pr_err("Couldn't get parent's regmap\n");
+		pr_err(&pdev->dev, "Couldn't get parent's regmap\n");
 		return -EINVAL;
 	}
 
