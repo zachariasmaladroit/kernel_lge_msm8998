a50df7d7544c5444d088e98672b4c0b3d2445355
zachariasmaladroit <zacharias.maladroit@gmail.com>
2018-12-25 02:21:09 +0100

    fs/proc/jolla-kernel: use existing "infrastructure" and update name
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>

--- fs/proc/jolla-kernel.c
+++ fs/proc/jolla-kernel.c
@@ -6,8 +6,8 @@
 
 static int jolla_kernel_proc_show(struct seq_file *m, void *v)
 {
-	seq_printf(m, "{\"kernel-name\": \"jolla_kernel_joan\","
-			"\"version\": \"v2.2\","
+	seq_printf(m, "{\"kernel-name\": \"pleroma_kernel_joan\","
+			"\"version\": \"I_WIP1\","
 			"\"buildtime\": \"%s\"}\n", JOLLA_KERNEL_TIMESTAMP);
 	return 0;
 }
