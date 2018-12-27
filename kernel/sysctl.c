abe68a1678f5ae76139453a95c6c0cadd26c4014
zachariasmaladroit <zacharias.maladroit@gmail.com>
2018-12-27 06:06:56 +0100

    sched_initial_task_util: not used, thus comment it out (compilation failure)
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -355,13 +355,13 @@ static struct ctl_table kern_table[] = {
 		.proc_handler	= proc_dointvec,
 	},
 #endif
-	{
+/*	{
 		.procname	= "sched_initial_task_util",
 		.data		= &sysctl_sched_initial_task_util,
 		.maxlen		= sizeof(unsigned int),
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec,
-	},
+	},*/
 	{
 		.procname	= "sched_cstate_aware",
 		.data		= &sysctl_sched_cstate_aware,
