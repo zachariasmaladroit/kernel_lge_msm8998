f6b982300ce7bd8a8c3480d44e1067515c94b47a
Yaroslav Furman <yaro330@gmail.com>
2018-12-30 01:17:52 +0300

    [eval] cpu_input_boost: don't kick frame boost when vidc is open[v2]
    As kerneltoast himself pointed out I should move this check to mdss.
    
    Same concept as in https://github.com/dev-harsh1998/kernel_lenovo_msm8916/commit/9cfe773270bed226c67f82de7ba731d386eab28d
    but instead of limiting max freq we will just reject all boost kicks.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    (cherry picked from commit 83b4c557a0d502528d473e18fbabe4fdae51181f)
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>
    
    Conflicts:
    	drivers/video/fbdev/msm/mdss_fb.c
    	include/linux/cpu_input_boost.h
    
    change pr_info() to pr_debug() to stop spamming logs
    
    (cherry picked from commit dd1abf6cc4c32e098fae43b9a708587c16fb9a65)
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>
    
    Conflicts:
    	drivers/video/fbdev/msm/mdss_fb.c
    	include/linux/cpu_input_boost.h

--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -5379,8 +5379,14 @@ int mdss_fb_do_ioctl(struct fb_info *info, unsigned int cmd,
 		ret = mdss_fb_mode_switch(mfd, dsi_mode);
 		break;
 	case MSMFB_ATOMIC_COMMIT:
-		cpu_input_boost_kick();
-		devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
+#ifdef CONFIG_CPU_INPUT_BOOST
+	if (!is_vidc_open()) {
+		if (time_before(jiffies, last_input_time + msecs_to_jiffies(5000))) {
+			cpu_input_boost_kick();
+			devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
+		}
+	}
+#endif
 		ret = mdss_fb_atomic_commit_ioctl(info, argp, file);
 		break;
 
