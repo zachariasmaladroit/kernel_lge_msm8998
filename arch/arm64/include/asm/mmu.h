b1f71589d534048b40c6897fef368d3413c59723
jollaman999 <admin@jollaman999.com>
2018-12-29 16:04:53 +0900

    Revert "arm64: remove dead code"
    This reverts commit b08d4640a3dca68670fc5af2fe9205b395a02388.

--- arch/arm64/include/asm/mmu.h
+++ arch/arm64/include/asm/mmu.h
@@ -71,6 +71,7 @@ static inline void arm64_apply_bp_hardening(void)	{ }
 #endif	/* CONFIG_HARDEN_BRANCH_PREDICTOR */
 
 extern void paging_init(void);
+extern void setup_mm_for_reboot(void);
 extern void __iomem *early_io_map(phys_addr_t phys, unsigned long virt);
 extern void init_mem_pgprot(void);
 extern void create_pgd_mapping(struct mm_struct *mm, phys_addr_t phys,
