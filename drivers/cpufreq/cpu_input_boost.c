f0f4895a2ab6112749f5de161ea3addae8697f01
Tyler Nijmeh <tylernij@gmail.com>
2018-12-29 18:32:11 -0800

    [eval|SQUASHED] cpu_input_boost: add and hook API for input timeout
    Sometimes, CPU boosting can be avoided if there is no user interaction ocurring.
    For example, general boosting will always boost on frame updates, regardless if the
    user physically interacts. This leads to excess power drain when performing non-interactive
    tasks such as video streaming. Developers must specify a timeout duration in milliseconds
    to compare the current jiffies against. If the last reported input exceeds the timeout
    duration, the function call will report false.
    
    This commit hooks this new API for MDSS frame update general boosting.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    (cherry picked from commit 89ce2d627b8e4be860ede9d94022671f4ec44ae9)
    
    video: mdss: make cpu input boost timeout modular
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    11c12b0c4ef2ba4e94683f96a631913b6926ee62
    
    video: mdss: allow tuning timeout ms via sysfs
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    52133f9466b7d52f87daa17b98ccfbe58c116809
    
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>
    
    Conflicts:
    	drivers/video/fbdev/msm/mdss_fb.c
    	include/linux/cpu_input_boost.h
    with last_input_time from previous commits (keep it in place)

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -52,9 +52,20 @@ struct boost_drv {
 	atomic64_t general_boost_expires;
 	atomic_t general_boost_dur;
 	atomic_t state;
+	atomic64_t prev_input_jiffies;
 };
 
 static struct boost_drv *boost_drv_g __read_mostly;
+
+bool cpu_input_boost_within_timeout(unsigned int input_boost_timeout) {
+	struct boost_drv *b = boost_drv_g;
+
+	if (!b)
+		return false;
+
+	return time_before(jiffies, atomic_read(&b->prev_input_jiffies)
+		+ msecs_to_jiffies(input_boost_timeout));
+}
 
 static u32 get_boost_freq(struct boost_drv *b, u32 cpu, u32 state)
 {
@@ -337,6 +348,7 @@ static void cpu_input_boost_input_event(struct input_handle *handle,
 	if (!(state & SCREEN_AWAKE))
 		return;
 
+	atomic64_set(&b->prev_input_jiffies, jiffies);
 	queue_work(b->wq, &b->input_boost);
 
 	last_input_time = jiffies;
