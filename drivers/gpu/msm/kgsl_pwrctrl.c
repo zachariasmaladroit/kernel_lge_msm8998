55d0c67d897007ce4801c2e25f3f5fd5e05656e0
Suprith Malligere Shankaregowda <supgow@codeaurora.org>
2019-01-01 04:02:50 +0100

    [partial] msm: kgsl: Fix XO shutdown issue
    This patch ensures device resumes successfully after
    XO shutdown without any KGSL error.
    
    Change-Id: I9eb8e281bc62793dc7521ba72aaeecf946860851
    Signed-off-by: Suprith Malligere Shankaregowda <supgow@codeaurora.org>
    
    this complements "BACKPORT: msm: kgsl: Keep dispatcher halted during
    SUSPEND state"/"adreno_dispatch: Use flush_kthread_worker() for
    msm-4.4 instead"
    
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -2605,6 +2605,7 @@ _aware(struct kgsl_device *device)
 		break;
 	default:
 		status = -EINVAL;
+		return status;
 	}
 	if (status)
 		kgsl_pwrctrl_request_state(device, KGSL_STATE_NONE);
@@ -2711,8 +2712,9 @@ static int _suspend(struct kgsl_device *device)
 {
 	int ret = 0;
 
-	if ((KGSL_STATE_NONE == device->state) ||
-			(KGSL_STATE_INIT == device->state))
+	if ((device->state == KGSL_STATE_NONE) ||
+			(device->state == KGSL_STATE_INIT) ||
+			(device->state == KGSL_STATE_SUSPEND))
 		return ret;
 
 	/* drain to prevent from more commands being submitted */
