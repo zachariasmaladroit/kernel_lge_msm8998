8c68a9750302ebea8ed4ea1966799605b83eb3d5
Raphiel Rollerscaperers <raphielscape@outlook.com>
2019-01-09 17:10:10 +0700

    [eval] drivers: staging: qcacld-3.0: relax every workqueue on freezable
    So it'll not unnecesairly waking up the device on sleep
    
    Signed-off-by: Raphiel Rollerscaperers <raphielscape@outlook.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>
    (cherry picked from commit 83745f4c51219f4ddc13e99d65eafbeefbf594eb)
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>
    
    Conflicts:
    	drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_assoc.c
    	drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_ipa.c
    	drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_p2p.c
    
    due to
    queue_delayed_work(system_power_efficient_wq,
    to
    queue_delayed_work(system_freezable_wq,
    conversion

--- drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_assoc.c
+++ drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_assoc.c
@@ -161,7 +161,7 @@ hdd_conn_set_authenticated(hdd_adapter_t *pAdapter, uint8_t authState)
 	pHddStaCtx->conn_info.uIsAuthenticated = authState;
 
 	/* Check is pending ROC request or not when auth state changed */
-	queue_delayed_work(system_power_efficient_wq,
+	queue_delayed_work(system_freezable_wq,
 			&pHddCtx->roc_req_work, 0);
 }
 
@@ -191,8 +191,8 @@ void hdd_conn_set_connection_state(hdd_adapter_t *adapter,
 	hdd_sta_ctx->conn_info.connState = conn_state;
 
 	if (conn_state != eConnectionState_NdiConnected)
-		queue_delayed_work(system_power_efficient_wq,
-				&hdd_ctx->roc_req_work, 0);
+		queue_delayed_work(system_freezable_wq,
+			&hdd_ctx->roc_req_work, 0);
 }
 
 /**
@@ -5204,7 +5204,8 @@ hdd_sme_roam_callback(void *pContext, tCsrRoamInfo *pRoamInfo, uint32_t roamId,
 		pAdapter->roam_ho_fail = false;
 		pHddStaCtx->ft_carrier_on = false;
 		complete(&pAdapter->roaming_comp_var);
-		schedule_delayed_work(&pHddCtx->roc_req_work, 0);
+		queue_delayed_work(system_freezable_wq,
+			&pHddCtx->roc_req_work, 0);
 		break;
 
 	default:
