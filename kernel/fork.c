8d02fce1a05d6e19c08bd186dae37f61fb8c83de
zachariasmaladroit <zacharias.maladroit@gmail.com>
2019-01-19 05:15:45 +0100

    fork: replace 'time_before(jiffies, last_input_time + msecs_to_jiffies())' with 'cpu_input_boost_within_timeout'
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -1844,7 +1844,7 @@ long _do_fork(unsigned long clone_flags,
 #ifdef CONFIG_CPU_INPUT_BOOST
 	/* Boost CPU to the max for 500 ms when userspace launches an app */
 	if (is_zygote_pid(current->pid) && !state_suspended &&
-		time_before(jiffies, last_input_time + msecs_to_jiffies(150))) {
+		cpu_input_boost_within_timeout(100)) {
 		cpu_input_boost_kick_max(500);
 		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 500);
 	}
