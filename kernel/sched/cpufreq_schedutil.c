99d18db97651097de7b359d9447c4e6380c16330
zachariasmaladroit <zacharias.maladroit@gmail.com>
2019-02-09 20:59:42 +0100

    Revert "[eval] sched: schedutil: keep quick ramp up, make ramp down quicker"
    This reverts commit 65434d70327f754da442a0e4eeaf4ceab678e403.

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -27,7 +27,7 @@ unsigned long boosted_cpu_util(int cpu);
 #define cpufreq_enable_fast_switch(x)
 #define cpufreq_disable_fast_switch(x)
 #define UP_RATE_LIMIT_US			(500)
-#define DOWN_RATE_LIMIT_US			(1000)
+#define DOWN_RATE_LIMIT_US			(20000)
 #define SUGOV_KTHREAD_PRIORITY	50
 
 struct sugov_tunables {
