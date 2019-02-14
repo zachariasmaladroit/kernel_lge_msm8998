f26f95278e0a32119303cf2267a7e8094509ecdf
zachariasmaladroit <zacharias.maladroit@gmail.com>
2019-02-14 02:02:10 +0100

    Revert "qcacld-3.0: Do not disable TDLS, during CSA from AP"
    This reverts commit cbe0113c67c4df5b0503bc56fadd4593497b9c12.

--- drivers/staging/qcacld-3.0/core/mac/src/pe/sch/sch_beacon_process.c
+++ drivers/staging/qcacld-3.0/core/mac/src/pe/sch/sch_beacon_process.c
@@ -792,10 +792,8 @@ static void __sch_beacon_process_for_session(tpAniSirGlobal mac_ctx,
 			 * delete all TDLS peers before leaving BSS and proceed
 			 * for channel switch
 			 */
-			if (LIM_IS_STA_ROLE(session)) {
-				session->is_tdls_csa = true;
+			if (LIM_IS_STA_ROLE(session))
 				lim_delete_tdls_peers(mac_ctx, session);
-			}
 
 			lim_update_channel_switch(mac_ctx, bcn, session);
 		} else if (session->gLimSpecMgmt.dot11hChanSwState ==
