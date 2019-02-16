846f42626563d3c8c7b4e323a54c0b7609dd4a2e
zachariasmaladroit <zacharias.maladroit@gmail.com>
2019-02-16 20:59:23 +0100

    Revert "cfq: Give a chance for arming slice idle timer in case of group_idle"
    This reverts commit 009adef8bfd0f88d6dd9edec34c3083151d5bc14.

--- block/cfq-iosched.c
+++ block/cfq-iosched.c
@@ -2973,8 +2973,7 @@ static void cfq_arm_slice_timer(struct cfq_data *cfqd)
 	 * for devices that support queuing, otherwise we still have a problem
 	 * with sync vs async workloads.
 	 */
-	if (blk_queue_nonrot(cfqd->queue) && cfqd->hw_tag &&
-		!cfqd->cfq_group_idle)
+	if (blk_queue_nonrot(cfqd->queue) && cfqd->hw_tag)
 		return;
 
 	WARN_ON(!RB_EMPTY_ROOT(&cfqq->sort_list));
