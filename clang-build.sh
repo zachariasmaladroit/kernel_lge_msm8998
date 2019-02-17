edd4dbb4eafcc6036df81571cab6a14965089cd6
zachariasmaladroit <zacharias.maladroit@gmail.com>
2019-02-17 22:37:34 +0100

    clang-build.sh: set up clang build-script properly
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>

--- clang-build.sh
+++ clang-build.sh
@@ -6,25 +6,28 @@
 #
 # Script to build a zImage from a kernel tree
 
-USE_CCACHE=1
-CACHE_DIR=~/.ccache
+#USE_CCACHE=1
+#CACHE_DIR=~/.ccache
 KBUILD_BUILD_USER=zacharias.maladroit
 KBUILD_BUILD_HOST=BuildHost
 
-PATH="~/android/toolchains/gcc-arm-8.2-2019.01-x86_64-aarch64-linux-gnu/bin:~/android/toolchains/clang_google/clang-r349610/bin:$PATH"
+#~/android/toolchains/gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu/bin:
+#CROSS_COMPILE=aarch64-linux-gnu- \
+
+export PATH="~/android/toolchains/clang_google/clang-r349610/bin:$PATH"
+export LD_LIBRARY_PATH="/home/matthias/android/toolchains/clang_google/clang-r349610/lib64:$LD_LIBRARY_PATH"
 
 # Kernel make function
 function kmake() {
 
-fakeroot make -j8 ARCH=arm64 CC="${CCACHE} clang" \
+make -j8 ARCH=arm64 CC=clang \
 CLANG_TRIPLE=aarch64-linux-gnu- \
-CROSS_COMPILE=aarch64-linux-gnu- \
-HOSTCC="${CCACHE} ~/android/toolchains/clang_google/clang-r349610/bin/clang" \
+DEFCONFIG=lineageos_h930_defconfig \
+VERBOSE=1 \
+HOSTCC="~/android/toolchains/clang_google/clang-r349610/bin/clang" \
 KBUILD_COMPILER_STRING="$(~/android/toolchains/clang_google/clang-r349610/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')" \
 "${@}"
 
 }
-
-kmake lineageos_h930_defconfig
 
 kmake
