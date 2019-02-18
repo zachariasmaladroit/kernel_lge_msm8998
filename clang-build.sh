d83a1a989c48c81fbfc6c6616473d2036bfc0c44
zachariasmaladroit <zacharias.maladroit@gmail.com>
2019-02-18 02:41:48 +0100

    clang-build.sh: more changes

--- clang-build.sh
+++ clang-build.sh
@@ -14,18 +14,22 @@ KBUILD_BUILD_HOST=BuildHost
 #~/android/toolchains/gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu/bin:
 #CROSS_COMPILE=aarch64-linux-gnu- \
 
-export PATH="~/android/toolchains/clang_google/clang-r349610/bin/:~/android/toolchains/aarch64-linux-android-4.9_google_03.2018/bin/:$PATH"
+export PATH="/home/matthias/android/toolchains/clang_google/clang-r349610/bin/:/home/matthias/android/toolchains/aarch64-linux-android-4.9_google_03.2018/bin/:$PATH"
 export LD_LIBRARY_PATH="/home/matthias/android/toolchains/clang_google/clang-r349610/lib64/:$LD_LIBRARY_PATH"
+export ARCH="arm64";
+export SUBARCH="arm64";
+export DEFCONFIG=lineageos_h930_defconfig
+source arch/${ARCH}/configs/$DEFCONFIG
 
 # Kernel make function
 function kmake() {
 
-make -j8 ARCH=arm64 CC=clang \
+KCFLAGS="-Wunused-variable" make -j8 ARCH=arm64 CC=clang \
 CLANG_TRIPLE=aarch64-linux-gnu- \
 CROSS_COMPILE=aarch64-linux-android- \
 VERBOSE=1 \
-HOSTCC="~/android/toolchains/clang_google/clang-r349610/bin/clang" \
-KBUILD_COMPILER_STRING="$(~/android/toolchains/clang_google/clang-r349610/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')" \
+HOSTCC="/home/matthias/android/toolchains/clang_google/clang-r349610/bin/clang" \
+KBUILD_COMPILER_STRING="$(/home/matthias/android/toolchains/clang_google/clang-r349610/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')" \
 "${@}"
 
 }
