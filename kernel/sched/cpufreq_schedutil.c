ff84e636bae8ece7a974de8591f824c4bd041247
idkwhoiam322 <snkm1999@gmail.com>
2019-03-31 12:25:30 +0530

    Revert "cpufreq: schedutil: switch uprate limit to 10ms"
    I can just adjust this baby in init.
    This reverts commit 91556612d8c99ff67a4c9b73900b227db5effc39.

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -782,7 +782,7 @@ static int sugov_init(struct cpufreq_policy *policy)
 	} else {
 		unsigned int lat;
 
-		tunables->up_rate_limit_us = 10000;
+		tunables->up_rate_limit_us = 500;
 		tunables->down_rate_limit_us = 20000;
 		lat = policy->cpuinfo.transition_latency / NSEC_PER_USEC;
 		if (lat) {
