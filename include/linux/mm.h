0a7c96a5ae0dfd9758bdddced947c34b29a04ba6
idkwhoiam322 <snkm1999@gmail.com>
2019-04-02 21:26:00 +0530

    Revert "BACKPORT: mm: Use overflow helpers in kvmalloc()"
    This reverts commit a6c33e1f99b911f37c450d29770fa9b02881ab0d.

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -21,7 +21,6 @@
 #include <linux/resource.h>
 #include <linux/page_ext.h>
 #include <linux/err.h>
-#include <linux/overflow.h>
 
 struct mempolicy;
 struct anon_vma;
@@ -430,12 +429,10 @@ static inline void *kvzalloc(size_t size, gfp_t flags)
 
 static inline void *kvmalloc_array(size_t n, size_t size, gfp_t flags)
 {
-	size_t bytes;
-
-	if (unlikely(check_mul_overflow(n, size, &bytes)))
+	if (size != 0 && n > SIZE_MAX / size)
 		return NULL;
 
-	return kvmalloc(bytes, flags);
+	return kvmalloc(n * size, flags);
 }
 
 extern void kvfree(const void *addr);
