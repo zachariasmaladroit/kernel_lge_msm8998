9673b403f87e0c9bdcd530420b8aa39b2d5c13df
idkwhoiam322 <snkm1999@gmail.com>
2019-04-02 21:26:26 +0530

    Revert "CHROMIUM: [media] v4l2-core: Use kvmalloc() for potentially big allocations"
    This reverts commit ac1f24e080f3a32ccf71247110ec064febc27c82.

--- drivers/media/v4l2-core/v4l2-event.c
+++ drivers/media/v4l2-core/v4l2-event.c
@@ -29,7 +29,6 @@
 #include <linux/sched.h>
 #include <linux/slab.h>
 #include <linux/export.h>
-#include <linux/mm.h>
 
 static unsigned sev_pos(const struct v4l2_subscribed_event *sev, unsigned idx)
 {
@@ -229,8 +228,7 @@ int v4l2_event_subscribe(struct v4l2_fh *fh,
 	if (elems < 1)
 		elems = 1;
 
-	sev = kvzalloc(sizeof(*sev) + sizeof(struct v4l2_kevent) * elems,
-		       GFP_KERNEL);
+	sev = kzalloc(sizeof(*sev) + sizeof(struct v4l2_kevent) * elems, GFP_KERNEL);
 	if (!sev)
 		return -ENOMEM;
 	for (i = 0; i < elems; i++)
@@ -316,10 +314,8 @@ int v4l2_event_unsubscribe(struct v4l2_fh *fh,
 	if (sev && sev->ops && sev->ops->del)
 		sev->ops->del(sev);
 
-
+	kfree(sev);
 	mutex_unlock(&fh->subscribe_lock);
-
-	kvfree(sev);
 
 	return 0;
 }
