eaab3b7f33db83a234880a4d33fe0a60667210ff
zachariasmaladroit <zacharias.maladroit@gmail.com>
2019-04-16 00:25:52 +0200

    Revert "msm: adsprpc: destroy mutex before file free"
    This reverts commit 8c5a0f0b07c6647511fbe1044e5fa3a4254d354b.

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -2293,7 +2293,8 @@ static int fastrpc_file_free(struct fastrpc_file *fl)
 	spin_unlock(&fl->apps->hlock);
 
 	if (!fl->sctx) {
-		goto bail;
+		kfree(fl);
+		return 0;
 	}
 	(void)fastrpc_release_current_dsp_process(fl);
 	spin_lock(&fl->hlock);
@@ -2311,8 +2312,6 @@ static int fastrpc_file_free(struct fastrpc_file *fl)
 		fastrpc_session_free(&fl->apps->channel[cid], fl->sctx);
 	if (fl->secsctx)
 		fastrpc_session_free(&fl->apps->channel[cid], fl->secsctx);
-bail:
-	mutex_destroy(&fl->map_mutex);
 	kfree(fl);
 	return 0;
 }
@@ -2324,7 +2323,7 @@ static int fastrpc_device_release(struct inode *inode, struct file *file)
 	if (fl) {
 		if (fl->debugfs_file != NULL)
 			debugfs_remove(fl->debugfs_file);
-
+		mutex_destroy(&fl->map_mutex);
 		fastrpc_file_free(fl);
 		file->private_data = NULL;
 	}
