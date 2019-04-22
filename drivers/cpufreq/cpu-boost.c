5ff27f564d3a2829445b308618f2681a158044fa
zachariasmaladroit <zacharias.maladroit@gmail.com>
2019-04-22 21:19:01 +0200

    Revert "cpufreq: cpu-boost: set interval between consecutive boosts to be at least the duration of the boost instead of a bigger value"
    This reverts commit 35ef2b7a6f63ae8a5d64bd525d87e79a148cf1bd.

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -45,6 +45,7 @@ static bool sched_boost_active;
 
 static struct delayed_work input_boost_rem;
 static u64 last_input_time;
+#define MIN_INPUT_INTERVAL (150 * USEC_PER_MSEC)
 
 static int set_input_boost_freq(const char *buf, const struct kernel_param *kp)
 {
@@ -393,7 +394,7 @@ static void cpuboost_input_event(struct input_handle *handle,
 	}
 
 	now = ktime_to_us(ktime_get());
-	if ((now - last_input_time) < (input_boost_ms * USEC_PER_MSEC))
+	if (now - last_input_time < MIN_INPUT_INTERVAL)
 		return;
 
 	if (work_pending(&input_boost_work))
