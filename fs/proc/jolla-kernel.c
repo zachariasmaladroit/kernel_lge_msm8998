c9708d0027788209f3a633b3db6bd02a0c82fe6c
zachariasmaladroit <zacharias.maladroit@gmail.com>
2019-04-28 05:47:46 +0200

    bump: I_t04-2

--- fs/proc/jolla-kernel.c
+++ fs/proc/jolla-kernel.c
@@ -7,7 +7,7 @@
 static int jolla_kernel_proc_show(struct seq_file *m, void *v)
 {
 	seq_printf(m, "{\"kernel-name\": \"marduk_kernel_joan\","
-			"\"version\": \"vI_t03_t4\","
+			"\"version\": \"vI_t04-2\","
 			"\"buildtime\": \"%s\"}\n", JOLLA_KERNEL_TIMESTAMP);
 	return 0;
 }
