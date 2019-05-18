8c452edc1af38d6792a76ba75ec99cf0bd37f2c1
zachariasmaladroit <zacharias.maladroit@gmail.com>
2019-05-18 05:19:46 +0200

    Revert "[squash] kernel: Boost all CPUs to the max when userspace launches an"
    This reverts commit d8bc22de696808897a07e987721df7306c99c927.

--- kernel/fork.c
+++ kernel/fork.c
@@ -1847,12 +1847,6 @@ long _do_fork(unsigned long clone_flags,
 		do_input_boost_max();
 #endif CONFIG_CPU_BOOST
 
-#ifdef CONFIG_CPU_INPUT_BOOST
-	/* Boost CPU to the max for 1250 ms when userspace launches an app */
-	if (is_zygote_pid(current->pid))
-		cpu_input_boost_kick_max(1250);
-#endif CONFIG_CPU_INPUT_BOOST
-
 	/*
 	 * Determine whether and which event to report to ptracer.  When
 	 * called from kernel_thread or CLONE_UNTRACED is explicitly
