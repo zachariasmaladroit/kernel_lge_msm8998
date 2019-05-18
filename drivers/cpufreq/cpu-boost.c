efec7335aaa5163ad01afc66dd7fb5683d7539e0
zachariasmaladroit <zacharias.maladroit@gmail.com>
2019-05-18 05:27:33 +0200

    Revert "[eval] cpu-boost: add a function to boost the cpu to max freq"
    This reverts commit 48064cd85b68b43f32076724aa1bf81447e684ae.

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -24,7 +24,6 @@
 #include <linux/time.h>
 #include <linux/kthread.h>
 #include <linux/sched/rt.h>
-#include <linux/cpu_boost.h>
 
 #include "../../kernel/sched/sched.h"
 
@@ -140,9 +139,6 @@ static int boost_adjust_notify(struct notifier_block *nb, unsigned long val,
 		if (!ib_min)
 			break;
 
-		ib_min = min((s->input_boost_min == UINT_MAX ?
-				policy->max : s->input_boost_min), policy->max);
-
 		pr_debug("CPU%u policy min before boost: %u kHz\n",
 			 cpu, policy->min);
 		pr_debug("CPU%u boost min: %u kHz\n", cpu, ib_min);
@@ -198,25 +194,6 @@ static void do_input_boost_rem(struct work_struct *work)
 	/* Update policies for all online CPUs */
 	update_policy_online();
 
-}
-
-void do_input_boost_max()
-{
-	unsigned int i;
-	struct cpu_sync *i_sync_info;
-
-	cancel_delayed_work_sync(&input_boost_rem);
-
-	for_each_possible_cpu(i) {
-		i_sync_info = &per_cpu(sync_info, i);
-		i_sync_info->input_boost_min = UINT_MAX;
-	}
-
-	update_policy_online();
-
-	queue_delayed_work(system_power_efficient_wq,
-		&input_boost_rem, msecs_to_jiffies(
-			!input_boost_ms ? 1500 : input_boost_ms));
 }
 
 static void do_input_boost(struct kthread_work *work)
