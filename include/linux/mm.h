8fafc50c9f975345a7e8ba3ff115cfbc57564de2
Tyler Nijmeh <tylernij@gmail.com>
2019-05-19 18:42:02 -0700

    mm: Allow readahead to be disabled
    Some solid state storage devices may be better off without disk
    readahead enabled. Originally, readahead was designed for rotational
    storage, where I/O was a massive bottleneck due to the overhead of
    spinning up the disk for seeks. For flash devices and SSDs, seek times
    are incredibly quick compared to rotational storage. These devices see
    very little gain from using readahead, and instead, experience increased
    I/O latency. Allow this option to be toggled by the kernel maintainer.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    (cherry picked from commit b337b1d50e18d8c3983e4fbd2e4f58fb988c90dc)
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>
    
    Conflicts:
    	include/linux/mm.h
    
    + 'block: Kconfig: create default min/max readahead configs'

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -1974,7 +1974,11 @@ int write_one_page(struct page *page, int wait);
 void task_dirty_inc(struct task_struct *tsk);
 
 /* readahead.c */
+#ifdef CONFIG_READAHEAD
 #define VM_MAX_READAHEAD CONFIG_VM_MAX_READAHEAD /* kbytes */
+#else
+#define VM_MAX_READAHEAD	0	/* kbytes */
+#endif
 #define VM_MIN_READAHEAD CONFIG_VM_MIN_READAHEAD /* kbytes (includes current page) */ 
 
 int force_page_cache_readahead(struct address_space *mapping, struct file *filp,
