173335bc3b0a5de09e6138c4482cbdba2ec5b1bb
zachariasmaladroit <zacharias.maladroit@gmail.com>
2019-05-23 02:43:05 +0200

    Revert "fs: sdfat: Update to version 2.1.8"
    This reverts commit bc047866c39651f8bee6d36bd7a1043b57f02896.

--- fs/sdfat/core_fat.c
+++ fs/sdfat/core_fat.c
@@ -1330,9 +1330,8 @@ static sector_t __calc_hidden_sect(struct super_block *sb)
 	hidden = bdev->bd_part->start_sect;
 	/* a disk device, not a partition */
 	if (!hidden) {
-		if (bdev != bdev->bd_contains)
-			sdfat_log_msg(sb, KERN_WARNING,
-				"hidden(0), but disk has a partition table");
+		ASSERT(bdev == bdev->bd_contains);
+		ASSERT(!bdev->bd_part->partno);
 		goto out;
 	}
 
@@ -1342,8 +1341,8 @@ static sector_t __calc_hidden_sect(struct super_block *sb)
 	}
 
 out:
-	sdfat_log_msg(sb, KERN_INFO, "start_sect of part(%d)    : %lld",
-		bdev ? bdev->bd_part->partno : -1, (s64)hidden);
+	sdfat_log_msg(sb, KERN_INFO, "start_sect of partition  : %lld",
+		(s64)hidden);
 	return hidden;
 
 }
