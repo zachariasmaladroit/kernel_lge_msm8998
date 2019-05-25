50c01cd2054df238660bb76e1f64f86cf86b5772
zachariasmaladroit <zacharias.maladroit@gmail.com>
2019-05-25 19:45:43 +0200

    [?] Revert "[EVAL|deps] sched/fair: Remove EAS enforcement in find_best_target()"
    This reverts commit 70400a84f903375602b5902c3379c92fd90fa47d.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6711,7 +6711,20 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 			}
 
 			/*
-			 * Favor CPUs with smaller capacity for Non latency
+			 * Enforce EAS mode
+			 *
+			 * For non latency sensitive tasks, skip CPUs that
+			 * will be overutilized by moving the task there.
+			 *
+			 * The goal here is to remain in EAS mode as long as
+			 * possible at least for !prefer_idle tasks.
+			 */
+			if ((new_util * capacity_margin) >
+			    (capacity_orig * SCHED_CAPACITY_SCALE))
+				continue;
+
+			/*
+			 * Favor CPUs with smaller capacity for non latency
 			 * sensitive tasks.
 			 */
 			if (capacity_orig > target_capacity)
