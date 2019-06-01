97cd1d040d67bccff435b766b6e7c10c9f6db3d6
zachariasmaladroit <zacharias.maladroit@gmail.com>
2019-06-01 22:14:32 +0200

    bump orion_II_WIP3_4-9 , we're on the new branch after all

--- fs/proc/jolla-kernel.c
+++ fs/proc/jolla-kernel.c
@@ -7,7 +7,7 @@
 static int jolla_kernel_proc_show(struct seq_file *m, void *v)
 {
 	seq_printf(m, "{\"kernel-name\": \"orion_kernel_joan\","
-			"\"version\": \"II_WIP3_4-8_t3_2\","
+			"\"version\": \"II_WIP3_4-9\","
 			"\"buildtime\": \"%s\"}\n", JOLLA_KERNEL_TIMESTAMP);
 	return 0;
 }
