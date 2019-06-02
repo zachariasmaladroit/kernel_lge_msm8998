057ee98493ac9ffd4990242c72b66bdd2e860f7f
zachariasmaladroit <zacharias.maladroit@gmail.com>
2019-06-03 01:07:49 +0200

    Revert "cpu_input_boost: add an option to choose a custom boost finish return freq for the LP cluster"
    This reverts commit 1ba6297e8f1ca622b5e005e53d97053d189b8fde.

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -241,7 +241,7 @@ static int cpu_notifier_cb(struct notifier_block *nb,
 		boost_freq = get_boost_freq(b, policy->cpu);
 		policy->min = min(policy->max, boost_freq);
 	} else {
-		policy->min = CONFIG_REMOVE_INPUT_BOOST_FREQ_LP;
+		policy->min = policy->cpuinfo.min_freq;
 	}
 
 	return NOTIFY_OK;
