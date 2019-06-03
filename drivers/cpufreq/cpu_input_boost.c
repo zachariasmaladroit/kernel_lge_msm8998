625045874c8e209bd9a7fbc46139be1cf731c89b
zachariasmaladroit <zacharias.maladroit@gmail.com>
2019-06-04 00:50:27 +0200

    Revert "[eval] cpu_input_boost: only boost on app launch if recently touched"
    This reverts commit b583b940677e3a35178ae881e94cd061cf7b0acb.

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -11,8 +11,6 @@
 #include <linux/input.h>
 #include <linux/kthread.h>
 
-unsigned long last_input_time;
-
 enum {
 	SCREEN_OFF,
 	INPUT_BOOST,
@@ -235,8 +233,6 @@ static void cpu_input_boost_input_event(struct input_handle *handle,
 	struct boost_drv *b = handle->handler->private;
 
 	__cpu_input_boost_kick(b);
-
-	last_input_time = jiffies;
 }
 
 static int cpu_input_boost_input_connect(struct input_handler *handler,
