687e7918c3c4e0f8171e6809d9d3532c459cd18d
zachariasmaladroit <zacharias.maladroit@gmail.com>
2019-06-06 02:07:41 +0200

    Revert "[LG joan] msm: mdss: Power on display asynchronously as early as"
    This reverts commit 71d3fd5566ffbe3fdf94218c7befe1816e5d8edc.

--- drivers/video/fbdev/msm/mdss_dsi.h
+++ drivers/video/fbdev/msm/mdss_dsi.h
@@ -663,18 +663,6 @@ struct mdss_dsi_ctrl_pdata {
 	struct dsi_panel_cmds bist_on_cmds;
 	struct dsi_panel_cmds bist_off_cmds;
 #endif
-
-	struct notifier_block wake_notif;
-	struct task_struct *wake_thread;
-	struct completion wake_comp;
-	wait_queue_head_t wake_waitq;
-	atomic_t disp_en;
-};
-
-enum {
-	MDSS_DISPLAY_OFF,
-	MDSS_DISPLAY_WAKING,
-	MDSS_DISPLAY_ON
 };
 
 struct dsi_status_data {
