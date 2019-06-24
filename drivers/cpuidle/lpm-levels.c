03a8c31a96bf31fe0961cc378a87a23b6a535a4b
zachariasmaladroit <zacharias.maladroit@gmail.com>
2019-06-24 16:33:58 +0200

    Revert "[eval|4.9] lpm-levels: Allow enforcing deepest cluster idle state"
    This reverts commit 91e2b130a543182182fe144679ddd3db95b67875.

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -70,9 +70,6 @@ static bool lpm_prediction = true;
 module_param_named(lpm_prediction,
 	lpm_prediction, bool, S_IRUGO | S_IWUSR | S_IWGRP);
 
-static bool cluster_use_deepest_state = true;
-module_param(cluster_use_deepest_state, bool, S_IRUGO | S_IWUSR | S_IWGRP);
-
 static uint32_t ref_stddev = 500;
 module_param_named(
 	ref_stddev, ref_stddev, uint, S_IRUGO | S_IWUSR | S_IWGRP
@@ -156,11 +153,6 @@ void lpm_suspend_wake_time(uint64_t wakeup_time)
 		suspend_wake_time = wakeup_time;
 }
 EXPORT_SYMBOL(lpm_suspend_wake_time);
-
-void lpm_cluster_use_deepest_state(bool enable)
-{
-	cluster_use_deepest_state = enable;
-}
 
 static uint32_t least_cluster_latency(struct lpm_cluster *cluster,
 					struct latency_level *lat_level)
@@ -997,26 +989,6 @@ static void clear_cl_predict_history(void)
 	}
 }
 
-static int cluster_select_deepest(struct lpm_cluster *cluster)
-{
-	int i;
-
-	for (i = cluster->nlevels - 1; i >= 0; i--) {
-		struct lpm_cluster_level *level = &cluster->levels[i];
-
-		if (level->notify_rpm) {
-			if (!(sys_pm_ops && sys_pm_ops->sleep_allowed))
-				continue;
-			if (!sys_pm_ops->sleep_allowed())
-				continue;
-		}
-
-		break;
-	}
-
-	return i;
-}
-
 static int cluster_select(struct lpm_cluster *cluster, bool from_idle,
 							int *ispred)
 {
@@ -1030,9 +1002,6 @@ static int cluster_select(struct lpm_cluster *cluster, bool from_idle,
 
 	if (!cluster)
 		return -EINVAL;
-
-	if (cluster_use_deepest_state)
-		return cluster_select_deepest(cluster);
 
 	sleep_us = (uint32_t)get_cluster_sleep_time(cluster, NULL,
 						from_idle, &cpupred_us);
--- drivers/cpuidle/lpm-levels.h
+++ drivers/cpuidle/lpm-levels.h
@@ -164,4 +164,3 @@ static inline bool is_IPI_pending(const struct cpumask *mask)
 	return false;
 }
 #endif
-void lpm_cluster_use_deepest_state(bool enable);
