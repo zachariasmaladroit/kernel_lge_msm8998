d89263c989dfe57813e6d99300a5638baca4370b
Tyler Nijmeh <tylernij@gmail.com>
2019-07-01 11:12:04 -0700

    ashmem: Align slab caches to L1 cache line
    Since these objects will be read repetatively, align them to the cache
    line for a higher chance at a cache hit.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    (cherry picked from commit 24a66896a665595609500619c3481655c2b45872)
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>

--- drivers/staging/android/ashmem.c
+++ drivers/staging/android/ashmem.c
@@ -813,7 +813,7 @@ static int __init ashmem_init(void)
 
 	ashmem_area_cachep = kmem_cache_create("ashmem_area_cache",
 					       sizeof(struct ashmem_area),
-					       0, 0, NULL);
+					       0, SLAB_HWCACHE_ALIGN, NULL);
 	if (unlikely(!ashmem_area_cachep)) {
 		pr_err("failed to create slab cache\n");
 		return -ENOMEM;
@@ -821,7 +821,7 @@ static int __init ashmem_init(void)
 
 	ashmem_range_cachep = kmem_cache_create("ashmem_range_cache",
 						sizeof(struct ashmem_range),
-						0, 0, NULL);
+						0, SLAB_HWCACHE_ALIGN, NULL);
 	if (unlikely(!ashmem_range_cachep)) {
 		pr_err("failed to create slab cache\n");
 		return -ENOMEM;
