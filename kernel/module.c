ff66bcf17033711ea3c203a8b1bcafa58783c805
zachariasmaladroit <zacharias.maladroit@gmail.com>
2019-07-22 20:20:31 +0200

    Revert "module: whitelist texfat external module"
    This reverts commit 81937fc77da726abd038bead6d09c9d32d9727bd.

--- kernel/module.c
+++ kernel/module.c
@@ -1296,7 +1296,7 @@ static int check_version(Elf_Shdr *sechdrs,
 	struct modversion_info *versions;
 
 	if(!strncmp("texfat", mod->name, 6))
-		return 1;
+		return 0;
 
 	/* Exporting module didn't supply crcs?  OK, we're already tainted. */
 	if (!crc)
