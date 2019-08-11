993cfc42d1c44a12b29336496368cf2eb62ae572
zachariasmaladroit <zacharias.maladroit@gmail.com>
2019-08-11 02:13:05 +0200

    Revert "diag: lge: Fix build error after 0407b306"
    This reverts commit 30631125e24b79eaa20d246625d9f1a2f8ce2f88.

--- drivers/char/diag/lg_diag_bypass.c
+++ drivers/char/diag/lg_diag_bypass.c
@@ -224,7 +224,7 @@ static void lge_bypass_close(struct tty_struct *tty, struct file *file)
 static int diag_bypass_request(const unsigned char* buf, int count)
 {
     if(diag_bypass_enable) {
-        diag_process_hdlc_pkt((void*)buf, (unsigned)count, 0);
+        diag_process_hdlc_pkt((void*)buf, (unsigned)count, NULL);
         return count;
     }
     return 0;
