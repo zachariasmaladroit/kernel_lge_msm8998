dcfefdb51e951b12239fd780aa49826c5d3896c0
zachariasmaladroit <zacharias.maladroit@gmail.com>
2019-08-11 02:13:30 +0200

    Revert "diag: Protect the decrement of number of diag clients"
    This reverts commit 349cd49f3204330d2ac45156286cd5d9aa82e486.

--- drivers/char/diag/diagchar_core.c
+++ drivers/char/diag/diagchar_core.c
@@ -393,8 +393,8 @@ static int diagchar_open(struct inode *inode, struct file *file)
 	return -ENOMEM;
 
 fail:
-	driver->num_clients--;
 	mutex_unlock(&driver->diagchar_mutex);
+	driver->num_clients--;
 	pr_err_ratelimited("diag: Insufficient memory for new client");
 	return -ENOMEM;
 }
