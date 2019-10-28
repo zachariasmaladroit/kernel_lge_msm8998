31a2a9620453f4dc7d38b510f375fec887d3a455
zachariasmaladroit <zacharias.maladroit@gmail.com>
2019-10-28 04:07:00 +0100

    Revert "arp: Implement arp_project for 4.4 kernel"
    This reverts commit 3d7b3771a312a845585405f7c48d7dc173a3c403.

--- include/net/arp_project.h
+++
@@ -1,8 +0,0 @@
-/* linux/net/arp_project.h */
-#ifndef _ARP_PROJECT_H
-#define _ARP_PROJECT_H
-
-#define ARP_PROJECT "arp_project: "
-#define ARP_PROJECT_VERSION "1.0"
-
-#endif	/* _ARP_PROJECT_H */
