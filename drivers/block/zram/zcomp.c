b5458f479dd6154edf351ed1b55154437b289057
zachariasmaladroit <zacharias.maladroit@gmail.com>
2019-11-19 00:52:58 +0100

    zram/zcomp: uncomment zstd [live-testing]
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>

--- drivers/block/zram/zcomp.c
+++ drivers/block/zram/zcomp.c
@@ -29,10 +29,10 @@ static const char * const backends[] = {
 #endif
 #if IS_ENABLED(CONFIG_CRYPTO_842)
 	"842",
-#endif
+#endif*/
 #if IS_ENABLED(CONFIG_CRYPTO_ZSTD)
 	"zstd",
-#endif*/
+#endif
 	NULL
 };
 
