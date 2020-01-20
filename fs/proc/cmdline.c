c08bef35934c47fb7c9ac572be0ed669fde84256
Sultan Alsawaf <sultan@kerneltoast.com>
2020-01-20 13:03:40 -0800

    [eval] proc: cmdline: Patch SafetyNet flags
    Userspace parses androidboot.* flags from /proc/cmdline and sets the
    ro.boot.* props accordingly, which in turn trips SafetyNet when the
    reported values are incorrect.
    
    Patch the cmdline flags checked by SafetyNet to prevent it from failing
    a device. These flags were found by extracting the latest snet.jar and
    searching for 'ro.boot.' strings.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    (cherry picked from commit b981f3623b7a075b3711c5d2659b4b890cee05d4)
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>
    
    Conflicts:
    	fs/proc/cmdline.c
    
    due to 'lge msm8998: export correct cmdline for offline charging'

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -7,6 +7,7 @@
 #include <linux/slab.h>
 
 static char updated_command_line[COMMAND_LINE_SIZE];
+static char new_command_line[COMMAND_LINE_SIZE];
 
 static void proc_cmdline_set(char *name, char *value)
 {
@@ -35,6 +36,7 @@ static int cmdline_proc_show(struct seq_file *m, void *v)
 	}
 #endif
 	seq_printf(m, "%s\n", updated_command_line);
+	seq_printf(m, "%s\n", new_command_line);
 	return 0;
 }
 
@@ -50,10 +52,40 @@ static const struct file_operations cmdline_proc_fops = {
 	.release	= single_release,
 };
 
+static void patch_flag(char *cmd, const char *flag, const char *val)
+{
+	size_t flag_len, val_len;
+	char *start, *end;
+
+	start = strstr(cmd, flag);
+	if (!start)
+		return;
+
+	flag_len = strlen(flag);
+	val_len = strlen(val);
+	end = start + flag_len + strcspn(start + flag_len, " ");
+	memmove(start + flag_len + val_len, end, strlen(end) + 1);
+	memcpy(start + flag_len, val, val_len);
+}
+
+static void patch_safetynet_flags(char *cmd)
+{
+	patch_flag(cmd, "androidboot.flash.locked=", "1");
+	patch_flag(cmd, "androidboot.verifiedbootstate=", "green");
+	patch_flag(cmd, "androidboot.veritymode=", "enforcing");
+}
+
 static int __init proc_cmdline_init(void)
 {
 	// copy it only once
 	strcpy(updated_command_line, saved_command_line);
+	strcpy(new_command_line, saved_command_line);
+
+	/*
+	 * Patch various flags from command line seen by userspace in order to
+	 * pass SafetyNet checks.
+	 */
+	patch_safetynet_flags(new_command_line);
 
 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
 	return 0;
